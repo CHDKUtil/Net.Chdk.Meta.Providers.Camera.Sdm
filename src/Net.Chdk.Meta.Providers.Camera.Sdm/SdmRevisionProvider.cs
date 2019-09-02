7fa74c3172f2c95fba66897341d2125fd8d3d7a9
Dmitry Shechtman <damnian@gmail.com>
2019-09-02 23:55:36 +0300

    Update GetRevision()

--- src/Net.Chdk.Meta.Providers.Camera.Sdm/SdmRevisionProvider.cs
+++ src/Net.Chdk.Meta.Providers.Camera.Sdm/SdmRevisionProvider.cs
@@ -1,6 +1,5 @@
 ﻿using Net.Chdk.Meta.Model.Camera.Ps;
 using Net.Chdk.Meta.Model.CameraList;
-using Net.Chdk.Meta.Model.CameraTree;
 using Net.Chdk.Meta.Providers.Camera.Ps;
 
 namespace Net.Chdk.Meta.Providers.Camera.Sdm
@@ -9,9 +8,9 @@ namespace Net.Chdk.Meta.Providers.Camera.Sdm
     {
         public override string ProductName => "SDM";
 
-        public override RevisionData GetRevision(string revision, TreeRevisionData treeRevision, ListPlatformData list, TreePlatformData tree)
+        protected override RevisionData GetRevision(string revision, ListRevisionData listRevision, ListPlatformData list)
         {
-            var key = treeRevision.Source?.Revision ?? revision;
+            var key = listRevision.Source?.Revision ?? revision;
             if (!list.Revisions.ContainsKey(key))
                 return null;
 
